78d8e50f0a40862bcd149ddc9c8b90630b0c1a28
Westermeister <westermeister12@gmail.com>
2021-07-27 15:26:14 -0700

    Add skeleton entry point

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Entry point for emulation.
+ * Copyright (C) 2021 Westermeister. All rights reserved.
+ */
+
+#include <iostream>
+#include <string>
+
+#include "logic.hpp"
+
+int main() { return 0; }
