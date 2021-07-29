fcc6cdc696f7799f4cb29ace5ea9096e6607636d
Westermeister <westermeister12@gmail.com>
2021-07-29 15:08:48 -0700

    Add complete multiplexer test suite

--- tests/mux.test.cpp
+++ tests/mux.test.cpp
@@ -6,3 +6,21 @@ TEST_CASE("Test mux::mux21") {
   REQUIRE(mux::mux21("01", "0") == "0");
   REQUIRE(mux::mux21("01", "1") == "1");
 }
+
+TEST_CASE("Test mux::mux41") {
+  REQUIRE(mux::mux41("0101", "00") == "0");
+  REQUIRE(mux::mux41("0101", "01") == "1");
+  REQUIRE(mux::mux41("0101", "10") == "0");
+  REQUIRE(mux::mux41("0101", "11") == "1");
+}
+
+TEST_CASE("Test mux::mux81") {
+  REQUIRE(mux::mux81("01010101", "000") == "0");
+  REQUIRE(mux::mux81("01010101", "001") == "1");
+  REQUIRE(mux::mux81("01010101", "010") == "0");
+  REQUIRE(mux::mux81("01010101", "011") == "1");
+  REQUIRE(mux::mux81("01010101", "100") == "0");
+  REQUIRE(mux::mux81("01010101", "101") == "1");
+  REQUIRE(mux::mux81("01010101", "110") == "0");
+  REQUIRE(mux::mux81("01010101", "111") == "1");
+}
